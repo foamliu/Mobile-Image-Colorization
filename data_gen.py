bf58b2c57c1b5e4e8727db78d22ca0c76b47319c
Yang Liu <liuyang12@focusmedia.cn>
2019-11-12 18:51:29 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -60,7 +60,6 @@ class MICDataset(Dataset):
         out_ab = out_lab[:, :, 1:].astype(np.int32) - 128
 
         y = get_soft_encoding(out_ab, self.nn_finder, self.nb_q)
-        print(y.shape)
 
         if np.random.random_sample() > 0.5:
             x = np.fliplr(x)
@@ -68,6 +67,9 @@ class MICDataset(Dataset):
 
         x = np.transpose(x, (2, 0, 1))  # HxWxC array to CxHxW
         y = np.transpose(y, (2, 0, 1))  # HxWxC array to CxHxW
+
+        print(x.shape)
+        print(y.shape)
 
         return x, y
 
