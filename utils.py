3e0f71947c9f48b97ab0a8647ef770ae4ae955ad
Yang Liu <liuyang12@focusmedia.cn>
2019-11-12 19:11:15 +0800

    update

--- utils.py
+++ utils.py
@@ -74,6 +74,8 @@ def get_learning_rate(optimizer):
 
 
 def accuracy(scores, targets, k=1):
+    print(scores.size())
+    print(targets.size())
     batch_size = targets.size(0)
     _, ind = scores.topk(k, 1, True, True)
     correct = ind.eq(targets.view(-1, 1).expand_as(ind))
