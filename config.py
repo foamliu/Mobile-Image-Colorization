ad2235a154dc3512fbcfef8953df2ecfc92ba8a6
Yang Liu <liuyang12@focusmedia.cn>
2019-11-12 17:30:57 +0800

    update

--- config.py
+++ config.py
@@ -6,12 +6,17 @@ im_size = 256
 channel = 3
 batch_size = 32
 epochs = 10000
-patience = 50
+
+# Training parameters
+num_workers = 1  # for data-loading; right now, only 1 works with h5py
+grad_clip = 5.  # clip gradients at an absolute value of
+print_freq = 100  # print training/validation stats  every __ batches
+checkpoint = None  # path to checkpoint, None if none
+
 num_train_samples = 529202
 num_valid_samples = 4268
 num_classes = 313
 kernel = 3
-weight_decay = 1e-3
 epsilon = 1e-8
 nb_neighbors = 5
 # temperature parameter T
