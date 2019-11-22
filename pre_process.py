7f3136814bab17567397077815fd6e9e0fd78a29
Yang Liu <liuyang12@focusmedia.cn>
2019-11-22 16:38:54 +0800

    udpate

--- pre_process.py
+++ pre_process.py
@@ -4,22 +4,23 @@ from config import image_folder, num_train
 
 
 def split_data():
-    names = [f for f in os.listdir(image_folder) if f.lower().endswith('.jpg')]
+    for split in ['train', 'val', 'test']:
+        folder = 'data/{}'.format(split)
+        files = [f for f in os.listdir(folder) if f.lower().endswith('.jpeg')]
 
-    num_samples = len(names)  # 118287
-    print('num_samples: ' + str(num_samples))
+        print('num_{}: {}'.format(split, len(files)))
 
-    train = names[:num_train]
-    valid = names[num_train:]
-
-    with open('train.txt', 'w') as file:
-        file.write('\n'.join(train))
-
-    with open('valid.txt', 'w') as file:
-        file.write('\n'.join(valid))
-
-    print('num_train: ' + str(len(train)))
-    print('num_valid: ' + str(len(valid)))
+        # train = names[:num_train]
+        # valid = names[num_train:]
+        #
+        # with open('train.txt', 'w') as file:
+        #     file.write('\n'.join(train))
+        #
+        # with open('valid.txt', 'w') as file:
+        #     file.write('\n'.join(valid))
+        #
+        # print('num_train: ' + str(len(train)))
+        # print('num_valid: ' + str(len(valid)))
 
 
 if __name__ == '__main__':
