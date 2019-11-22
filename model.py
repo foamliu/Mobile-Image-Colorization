37ddba9bd35e3f4d4183474164b3cb90a4e8f280
Yang Liu <liuyang12@focusmedia.cn>
2019-11-22 18:00:25 +0800

    update

--- model.py
+++ model.py
@@ -1,21 +1,20 @@
 import torch
-from torchvision import models
-from models.deeplab import DeepLab
 from torchscope import scope
+
 from config import num_classes
+from models.deeplab import DeepLab
 
 if __name__ == "__main__":
-    # model = DeepLab(backbone='mobilenet', output_stride=16, num_classes=num_classes)
-    # model.eval()
-    # input = torch.rand(1, 3, 256, 256)
-    # output = model(input)
-    # print(output.size())
-    # scope(model, (3, 256, 256))
-
-    model = models.segmentation.deeplabv3_resnet101(pretrained=True, num_classes=num_classes)
+    model = DeepLab(backbone='mobilenet', output_stride=16, num_classes=num_classes)
     model.eval()
     input = torch.rand(1, 3, 256, 256)
-    output = model(input)['out']
+    output = model(input)
     print(output.size())
     scope(model, (3, 256, 256))
 
+    # model = models.segmentation.deeplabv3_resnet101(pretrained=True, num_classes=num_classes)
+    # model.eval()
+    # input = torch.rand(1, 3, 256, 256)
+    # output = model(input)['out']
+    # print(output.size())
+    # scope(model, (3, 256, 256))
